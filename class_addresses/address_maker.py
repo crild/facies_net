59d613b39f84477ef0fffbfc433b176eb393960a
Charles Rutherford Ildstad <32792378+crild@users.noreply.github.com>
2018-05-07 17:40:59 +0000

    Update address_maker.py

--- class_addresses/address_maker.py
+++ class_addresses/address_maker.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 # Function that creates .pts files for training
-def data_creator(corner_coord,sample_rates,save_name):
+def data_creator(corner_coords,sample_rates,save_name):
     # corner_coordinates: cordinates for the top left and bottom right points
     # sample_rate: inline, xline, time sample rates
 
@@ -13,37 +13,54 @@ def data_creator(corner_coord,sample_rates,save_name):
     xline_step = sample_rates[1]
     time_step = sample_rates[2]
 
-    inline_length = int((corner_coord[1,0]-corner_coord[0,0])//inline_step)
-    xline_length = int((corner_coord[1,1]-corner_coord[0,1])//xline_step)
-    time_length = int((corner_coord[1,2]-corner_coord[0,2])//time_step)
+    # Define total section counter:
+    sec_len = 0
+
+    arr = np.empty((0,3))
+
+    for corner_coord in corner_coords:
+        # Define some limiting parameters
+        inline_length = int((corner_coord[1,0]-corner_coord[0,0])//inline_step)+1
+        xline_length = int((corner_coord[1,1]-corner_coord[0,1])//xline_step)+1
+        time_length = int((corner_coord[1,2]-corner_coord[0,2])//time_step)+1
 
 
-    inl_range = np.arange(corner_coord[0,0],corner_coord[1,0]+inline_step,inline_step)
-    xl_range = np.arange(corner_coord[0,1],corner_coord[1,1]+xline_step,xline_step)
-    t_range = np.arange(corner_coord[0,2],corner_coord[1,2]+time_step,time_step)
+        inl_range = np.arange(corner_coord[0,0],corner_coord[1,0]+inline_step,inline_step)
+        xl_range = np.arange(corner_coord[0,1],corner_coord[1,1]+xline_step,xline_step)
+        t_range = np.arange(corner_coord[0,2],corner_coord[1,2]+time_step,time_step)
 
-    out_array = np.empty(((inline_length+1)*(xline_length+1)*(time_length+1),3))
+        out_array = np.empty((inline_length*xline_length*time_length,3))
 
-    for inl in inl_range:
-        i = int((inl - corner_coord[0,0])//inline_step)
-        for xl in xl_range:
-            j = int((xl - corner_coord[0,1])//xline_step)
-            for tim in t_range:
-                k = int((tim - corner_coord[0,2])//time_step)
-                out_array[i*inline_length+j*xline_length+k] = [inl,xl,tim]
+        for inl in inl_range:
+            i = int((inl - corner_coord[0,0])//inline_step)
+            for xl in xl_range:
+                j = int((xl - corner_coord[0,1])//xline_step)
+                for tim in t_range:
+                    k = int((tim - corner_coord[0,2])//time_step)
+                    print(i,j,k)
+                    print(xline_length,time_length)
+                    out_array[i*xline_length*time_length+j*time_length+k] = [inl,xl,tim]
+
+        arr = np.append(arr,out_array,axis = 0)
 
     # Save csv file
-    np.savetxt(fname = save_name+'.pts',X = out_array,fmt = '%f')
+    np.savetxt(fname = save_name+'.pts',X = arr,fmt = '%i')
 
 
 # Define the function parameters
-top_left = [100,200,44.4]
-bot_right = [106,200,52.4]
+top_left1 = [100,200,44.4]
+bot_right1 = [106,200,52.4]
+
+top_left2 = [88,100,44.4]
+bot_right2 = [96,100,52.4]
 
 samp_rt = np.array([2,2,4])
 
 name = 'test'
 
 # Set the parameters correctly and run the function
-c_cord = np.array([top_left,bot_right])
+c_cord = np.array([[top_left1,bot_right1],
+                  [top_left2,bot_right2]])
+
+
 data_creator(c_cord,samp_rt,name)
